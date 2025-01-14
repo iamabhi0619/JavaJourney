578528da5b683a68643d6ccf8777e357dcf05da7
Abhishek Kumar <iamabhi0619@gmail.com>
2025-01-14 21:00:16 +0530

    Done with Day01

---
+++ basic/NewDataType.java
@@ -0,0 +1,20 @@
+public class NewDataType {
+    public static void main(String args []){
+        //this works after java-9
+        // There is no way before without defining the dataType of variable
+
+        int i = 19;
+        double d = 12.6;
+        String s = "Hello Java world";
+        boolean b = true;
+
+        // var v3 = "Hello Java";
+        // System.out.println(v3);
+
+
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s);
+        System.out.println(b);
+    }
+}
---
+++ basic/arithmetic.java
@@ -0,0 +1,17 @@
+public class arithmetic {
+    public static void main(String args[]) {
+        int v1 = 5, v2 = 6;
+        System.out.println(v1 + v2);
+        System.out.println(v1 - v2);
+        System.out.println(v1 * v2);
+        System.out.println(v1 / v2);
+        System.out.println(v1 % v2);
+
+        // + oprater is also used to concate the string
+
+        String s1 = "Hello";
+        String s2 = "Abhishek";
+
+        System.out.println(s1 + " " + s2);
+    }
+}
